d873b40cc50a8778127bc671bc68b000043e96bc
Erik Nunes <erikvixx@gmail.com>
2024-03-13 17:51:04 -0300

    fea: include dashboard charts

--- src/pages/all-property.tsx
+++ src/pages/all-property.tsx
@@ -1,5 +1,16 @@
 import React from "react";
-
+import { Add } from "@mui/icons-material";
+import { useList } from "@refinedev/core";
+import { Box, Stack, Typography } from "@mui/material";
+import { useNavigation } from "@refinedev/core";
+import CustomButtonfrom from "../components/common/CustomButton";
+import PropertyCardfrom from "../components/common/PropertyCard";
 export default function allProperty() {
-  return <div>all-property</div>;
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        Dashboard
+      </Typography>
+    </Box>
+  );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -14,7 +14,7 @@ export default function home() {
       <Typography fontSize={25} fontWeight={700} color="#11142d">
         Dashboard
       </Typography>
-      <Box mt="20px" display="flex" flexWrap="wrap" gap="4">
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
         <PieChart
           title="Properties for Sale"
           value={684}
@@ -40,7 +40,12 @@ export default function home() {
           colors={["#475be8", "#e4e8ef"]}
         />
       </Box>
-      <Stack mt={25} width={"100%"} direction={{ xs: "column", lg: "row" }}>
+      <Stack
+        mt="25px"
+        width="100%"
+        direction={{ xs: "column", lg: "row" }}
+        gap={4}
+      >
         <TotalRevenue />
         <PropertyReferrals />
       </Stack>
